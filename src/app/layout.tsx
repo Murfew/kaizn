062891aca0a5a9c37707ecc25ee8b067c6bf7de4
Murfew <matthew.hurley953@gmail.com>
2026-06-14 16:31:45 -0400

    feat: wire up fonts to root layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { env } from "@/env";
+import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geist = Geist({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-sans",
+});
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-mono",
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={`${geist.variable} ${geistMono.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
